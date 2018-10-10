b3594dc5b6a324100397c47a53e845c51c9c5445
Taylor Devereaux <taylordevereaux@live.ca>
2018-10-10 19:18:20 -0300

    adds schedul, creates recurring dates

---
+++ CashFlow.Api/Models/Schedule.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashFlow.Api.Models
+{
+    public class Schedule
+    {
+        public DateTime StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public int RecursEveryAmount { get; set; } = 1;
+
+        public RecurrenceType RecurrenceType { get; set; }
+        [MinLength(1)]
+        [MaxLength(28)]
+        public int? DayOfMonth { get; set; }
+
+        public Ordinal? Ordinal { get; set; }
+
+        public DayOfWeek? DayOfWeek { get; set; }
+    }
+
+    public enum RecurrenceType
+    {
+        Daily,
+        Weekly,
+        Monthly,
+        Yearly
+    }
+
+    public enum Ordinal
+    {
+        First,
+        Second,
+        Third,
+        Forth,
+        Last
+    }
+}
