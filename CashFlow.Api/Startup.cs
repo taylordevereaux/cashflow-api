65e9fc34ec66b09698f6836e5af40224e6bd3dc1
Taylor Devereaux <taylordevereaux@live.ca>
2018-10-11 21:05:12 -0300

    fix: DB Connection string in scaffolded DBContext was not being read.

--- CashFlow.Api/Startup.cs
+++ CashFlow.Api/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -35,7 +36,9 @@ namespace CashFlow.Api
                     })
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.AddScoped<CashFlowDBContext>();
+            services.AddDbContext<CashFlowDBContext>(options =>
+                options.UseSqlServer(Configuration["DBConnectionString"]));
+            //services.AddScoped<CashFlowDBContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
